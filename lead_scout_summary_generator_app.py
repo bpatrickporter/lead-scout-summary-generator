49f96e2bb192429e86edb7d42e3ec857f68c7720
trayls <tylererayls@gmail.com>
2025-06-13 08:13:11 -0400

    remove unused imports

--- lead_scout_summary_generator_app.py
+++ lead_scout_summary_generator_app.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
-from datetime import datetime, timedelta
-import pytz
-import plotly.express as px
-from geopy.geocoders import ArcGIS
-from geopy.extra.rate_limiter import RateLimiter
-from collections import defaultdict
 
 def main():
     acculynx = st.Page("./pages/acculynx.py", title="Acculynx Report Generator", icon=":material/paid:")
@@ -15,7 +6,6 @@ def main():
     pg = st.navigation([acculynx, lead_scout])
     st.set_page_config(page_title="📊 Report Generator", page_icon=":material/edit:", layout="wide")
     pg.run()
-
 
 if __name__ == "__main__":
     main()
