b34c569d87774bd56539914262b11dfc125c84de
trayls <tylererayls@gmail.com>
2025-06-19 07:22:34 -0400

    get user and pass from env

--- adapter/email/acculynx.py
+++ adapter/email/acculynx.py
@@ -1,11 +1,15 @@
 # This file is an adapter to gather reports in csv files from email attachments. 
 # Each morning a report is sent to the user, and this adapter processes that report.
 import imaplib
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class AccuLynxEmailAdapter:
-    def __init__(self, email_user, email_password):
-        self.email_user = email_user
-        self.email_password = email_password
+    def __init__(self):
+        self.email_user = os.getenv('email-username')
+        self.email_password = os.getenv('email-password')
         self.imap_server = "imap-mail.outlook.com"  # Replace with actual IMAP server
         self.mail = None
         
@@ -17,15 +21,4 @@ class AccuLynxEmailAdapter:
 
     def fetch_reports(self):
         """Fetch reports from email attachments."""
-        result, data = self.mail.search(None, "ALL")
-        email_ids = data[0].split()
-        reports = []
-
-        for email_id in email_ids:
-            result, msg_data = self.mail.fetch(email_id, "(RFC822)")
-            msg = email.message_from_bytes(msg_data[0][1])
-            for part in msg.walk():
-                if part.get_content_type() == "application/vnd.ms-excel":
-                    reports.append(part.get_payload(decode=True))
-
-        return reports
+        return
